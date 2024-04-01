6c9bd25c5a15f6f29c9a2d93f2aa7499f37b6cac
DajmondFM <franekmalikowski@gmail.com>
2024-04-01 02:41:19 +0200

    Update 2

--- lua/vim.lua
+++ lua/vim.lua
@@ -10,9 +10,12 @@ vim.cmd("set mouse=a")
 vim.cmd("set noshowmode")
 vim.opt.virtualedit = "onemore"
 vim.opt.guicursor = "n-i-c:ver100,v:blinkwait300-blinkon250-blinkoff200"
+vim.keymap.set('n', '<C-s>', '<Cmd>w<CR>')
+
 -- Kropki w miejscu spacji
 vim.cmd("set list")
 vim.cmd("set lcs+=space:·")
+
 -- DZIWNE KOMENDY
 --vim.cmd("set nofsync")
 -- vim.cmd("set noswapfile")
