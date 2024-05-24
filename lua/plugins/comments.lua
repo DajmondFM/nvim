4de8b87a3ee33a0abd8d0231b4b390b8c184b4a9
DajmondFM <franekmalikowski@gmail.com>
2024-05-25 00:13:29 +0200

    Update 6

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -12,5 +12,8 @@ return {
      })
     vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
     vim.keymap.set("v", "<C-_>", "gc", { remap = true })
-   end
+    vim.keymap.set("n", "<C-/>", "gcc", { remap = true })
+    vim.keymap.set("v", "<C-/>", "gc", { remap = true })
+ 
+  end
 }
