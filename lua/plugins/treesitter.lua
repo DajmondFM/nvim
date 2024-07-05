925f7da12c1f1af6709af3eb7072ba9096962e34
DajmondFM <franekmalikowski@gmail.com>
2024-07-05 13:05:01 +0200

    Update 8.1

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup {
-        ensure_installed = {"lua", "rust", "python", "toml", "json", "html", "css", "cpp", }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        ensure_installed = {"lua", "rust", "python", "toml", "json", "html", "css", "cpp", "c", }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
         -- ensure_installed = "all",
         exclude = { "" },
         highlight = { enable = true},
