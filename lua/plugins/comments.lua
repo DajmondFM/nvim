1b061cef6bbac9bad6fa86ea938b522e82dac337
Dajmond <franekmalikowski@gmail.com>
2024-07-16 17:34:49 +0200

    Update 8.9.3

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -3,14 +3,14 @@ return {
   event = "VeryLazy",
   lazy = false,
   config = function()
-     require("Comment").setup({
-       toggler = {
-         ---Line-comment toggle keymap
-           line = 'gcc',
-         ---Block-comment toggle keymap
-           block = 'gbc',
-       }
-     })
+    require("Comment").setup({
+      toggler = {
+        ---Line-comment toggle keymap
+        line = 'gcc',
+        ---Block-comment toggle keymap
+        block = 'gbc',
+      }
+    })
     vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
     vim.keymap.set("v", "<C-_>", "gc", { remap = true })
     vim.keymap.set("n", "<C-/>", "gcc", { remap = true })
--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,15 +1,37 @@
-return {
-  "lukas-reineke/indent-blankline.nvim", 
-  event =  "VeryLazy",
-  main = "ibl",
+-- return {{
+--   "lukas-reineke/indent-blankline.nvim", 
+--   event =  "VeryLazy",
+--   main = "ibl",
+--   config = function()
+--     require("ibl").setup({
+--       indent = { char = "▏",},
+--       --buftype_exclude = {"terminal"},
+--       scope = { enabled = false },
+--       --highlight = { "Function", "Label" },    
+--
+--     })
+--
+--   end
+-- },
+--
+return{
+  "shellRaining/hlchunk.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("ibl").setup({
-      indent = { char = "▏",},
-      --buftype_exclude = {"terminal"},
-      scope = { enabled = false },
-      --highlight = { "Function", "Label" },    
-
-    })
-
-  end
+    require('hlchunk').setup({
+      chunk = {
+        enable = true
+        -- ...
+      },
+      indent = {
+        enable = true,
+        chars = { "│" },
+        use_treesitter = true,
+        -- ...
+      },
+      line_num = {
+        enable = true
+        -- ...
+      }
+    })  end
 }
