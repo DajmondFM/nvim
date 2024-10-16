352010f968f7cace309d1d101adc532c25ba3af8
Dajmond <franekmalikowski@gmail.com>
2024-10-16 21:19:49 +0200

    Update TEST

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,6 +18,7 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        build = ":MasonUpdate",
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
