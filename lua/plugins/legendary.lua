f26445ae01da3ef0b61b52ac414f51c78f7cac72
Dajmond <franekmalikowski@gmail.com>
2025-01-21 01:15:29 +0100

    Update 9.9.1

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -7,7 +7,7 @@ return {
   lazy = false,
   -- enabled = false,
   -- sqlite is only needed if you want to use frecency sorting
-  -- dependencies = { 'kkharji/sqlite.lua' }
+  dependencies = { 'kkharji/sqlite.lua' },
 
   config = function ()
     require('legendary').setup ({
